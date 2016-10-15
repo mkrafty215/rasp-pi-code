a1a1dde07c60ab06a86e702cbd330a7dafe156ea
MK.01 <mkraft100@gmail.com>
2016-10-15 14:23:59 -0400

    program start; kb interrupt; setup fn
    Shell of executing script, loop still missing, this is WIP.

--- executor.py
+++ executor.py
@@ -1,1 +1,26 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
 
+xPin = 0
+
+def setup():
+  GPIO.setmode(GPIO.BOARD)    # Numbers GPIOs by physical location
+  GPIO.setup(xPin, GPIO.OUT)
+  
+	p = GPIO.PWM(xPin, 1000)    # set Frequece to 1KHz
+	p.start(0)                    # Duty Cycle = 0
+  
+  #
+  # Loop - WIP
+  #
+  
+def destroy():
+	GPIO.output(xPin, GPIO.HIGH)     # led off
+	GPIO.cleanup()                     # Release resource
+
+if __name__ == '__main__':     # Program start from here
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		destroy()
